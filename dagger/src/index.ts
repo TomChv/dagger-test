84e7d0594f0ae1b30c332eda4461c4f8770df931
Vasek - Tom C <tom.chauveau@epitech.eu>
2024-06-26 11:54:05 +0200

    chore: add contextual file loading
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- dagger/src/index.ts
+++ dagger/src/index.ts
@@ -1,9 +1,11 @@
-import { Directory, object, func, context } from "@dagger.io/dagger"
+import { Directory, object, func, context, File } from "@dagger.io/dagger"
 
 @object()
 class Context {
   @func()
-  async test(@context("/") dir: Directory): Promise<string[]> {
-    return dir.entries()
+  async test(@context("/") dir: Directory, @context("/dagger.json") conf: File): Promise<string[]> {
+    const res = await dir.entries()
+
+    return [...res, await conf.contents()]
   }
 }
